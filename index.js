9ed9613ac21af6a11f5fedddb7aeeed9a8becb90
Slutishboy <sohomsaha361@gmail.com>
2024-02-17 02:09:35 +0530

    api-setup

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const cron = require("node-cron");
+const {run}=require('./sendMail');
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const port = 3000
+var list=[]
+app.use(express.json())
+app.post('/sc', (req, res) => {
+  let url=req.body.url;
+  let price=req.body.price;
+  let email=req.body.email;
+  let timeout=3
+  list.push({url,price,email,timeout});  
+  res.send('Success')
 })
+
+cron.schedule("*/15 * * * * *",()=> {
+    for(var i=0;i<list.length;i++){
+       run(list[i].url,list[i].price,list[i].email);
+       list[i].timeout--;
+    }
+    list = list.filter(function(item) {
+        return item.timeout !== 0
+    })
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- sendMail.js
+++ sendMail.js
@@ -3,19 +3,24 @@ const cheerio  = require('cheerio');
 
 const puppeteer = require('puppeteer');
 
-const run=async ()=>{
+
+async function run(url,price,email)
+{
 try {
   // Launch the browser
   const browser = await puppeteer.launch();
 
   // Create a page
   const page = await browser.newPage();
-  await page.goto('https://www.amazon.in/WildHorn-Blue-Mens-Wallet-WH2009/dp/B076ZMLBJJ/?_encoding=UTF8&pd_rd_w=Cms2p&content-id=amzn1.sym.5ee8c642-34d6-4cfc-933c-7214ec67ba53%3Aamzn1.symc.acc592a4-4352-4855-9385-357337847763&pf_rd_p=5ee8c642-34d6-4cfc-933c-7214ec67ba53&pf_rd_r=0545N47APYMAPG5JH3RZ&pd_rd_wg=3R7oM&pd_rd_r=e7ee5975-d447-49c8-83cd-0940e05ec230&ref_=pd_gw_ci_mcx_mr_hp_d&th=1'); 
- const htmlContent=await page.content();
-  const $=cheerio.load(htmlContent);
-  const temp=$('.a-price-whole');
-  const value=parseFloat(temp[0].children[0].data)
-  console.log(value);
+  await page.goto(url); 
+ var htmlContent=await page.content();
+  var $=cheerio.load(htmlContent);
+  var temp=$('.a-price-whole').text();
+  temp=temp.split(".");
+  temp=parseFloat(temp[0].replace(/,/g, ''))  
+//   const value=parseFloat(temp[0].children[0].data)
+//   console.log(value);
+  console.log(temp);
   
 
   // Close browser.
@@ -25,4 +30,4 @@ catch(e){
     console.log(e);
 }
 }
-run()
+module.exports={run};
