d29f1b9096f84b09abe6834913ef68a24ea66bb4
Slutishboy <sohomsaha361@gmail.com>
2024-02-24 23:18:08 +0530

    udate

--- sendMail.js
+++ sendMail.js
@@ -2,10 +2,10 @@
 const cheerio  = require('cheerio');
 const sgMail=require("@sendgrid/mail");
 // if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  const chrome = require("chrome-aws-lambda");
- const puppeteer = require("puppeteer-core");
+  // const chrome = require("chrome-aws-lambda");
+//  const puppeteer = require("puppeteer-core");
 // } else {
-  // const puppeteer = require("puppeteer");
+  const puppeteer = require("puppeteer");
 // }
 require('dotenv').config();
 
